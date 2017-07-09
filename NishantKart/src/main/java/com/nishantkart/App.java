8f7f6d7400275394ee6b8ac05683beaaf45917ec
Nishant Burdhan <nishant.burdhan@gmail.com>
2017-07-09 20:06:17 +0530

    Create Project

---
+++ NishantKart/src/main/java/com/nishantkart/App.java
@@ -0,0 +1,13 @@
+package com.nishantkart;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
